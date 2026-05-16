007b9c35783eba733061c19bdaf2b6713aa3d03b
praneesh07 <praneeshvijay31@gmail.com>
2026-05-16 22:01:09 +0530

    change the saize of the padding in mail

--- src/App.tsx
+++ src/App.tsx
@@ -324,7 +324,7 @@ export default function App() {
                     href="mailto:praneeshgaran@gmail.com"
                     className="text-gray-300 transition-colors duration-300"
                   >
-                    <Mail className="w-4 h-4" />
+                    <Mail className="w-5 h-5" />
                   </motion.a>
 
                   <motion.a
